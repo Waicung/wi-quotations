a6f1d8586829f0cdddcaaa93275120d7c963de6e
Waicung F <better@waicung.net>
2024-10-08 11:11:22 +1100

    view

--- app/middleware.php
+++ app/middleware.php
@@ -4,7 +4,15 @@ declare(strict_types=1);
 
 use App\Application\Middleware\SessionMiddleware;
 use Slim\App;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 return function (App $app) {
     $app->add(SessionMiddleware::class);
+
+    // Create Twig
+    $twig = Twig::create(__DIR__ . '/../templates', ['cache' => false]);
+
+    // Add Twig-View Middleware
+    $app->add(TwigMiddleware::create($app, $twig));
 };
--- app/routes.php
+++ app/routes.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
-use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 use App\Quotations\Printer;
+use Slim\Views\Twig;
 
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
@@ -15,33 +15,18 @@ return function (App $app) {
     });
 
     $app->get('/', function (Request $request, Response $response) {
-        $html = '<html><body><h1>Hello world!</h1>';
-        $html .= '<form action="/generate-pdf" method="post">';
-        $html .= '<button type="submit">生成并下载PDF</button>';
-        $html .= '</form></body></html>';
-        $response->getBody()->write($html);
-        return $response;
+        $view = Twig::fromRequest($request);
+        return $view->render($response, 'index.twig');
     });
 
     $app->post('/generate-pdf', function (Request $request, Response $response) {
         // 获取发票 HTML
 
-        $pdfContent = Printer::print('simple', [
-            'invoiceNumber' => 'INV-123456',
-            'invoiceDate' => '2020-01-01',
-            'dueDate' => '2020-01-31',
-            'total' => 1000,
-            'tax' => 50,
-            'grandTotal' => 1050,
-            'items' => [
-                ['description' => 'Item 1', 'quantity' => 2, 'price' => 250, 'total' => 500],
-                ['description' => 'Item 2', 'quantity' => 1, 'price' => 500, 'total' => 500],
-            ],
-        ]);
+        $pdfContent = Printer::print('simple', []);
 
         // 设置响应头，使浏览器将其识别为下载的 PDF 文件
         $response = $response->withHeader('Content-Type', 'application/pdf')
-            ->withHeader('Content-Disposition', 'attachment; filename="invoice.pdf"');
+            ->withHeader('Content-Disposition', 'attachment; filename="wi-quotations.pdf"');
 
         $response->getBody()->write($pdfContent);
         return $response;
